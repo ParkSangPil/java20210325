41a8f2db9107163da92d4f6df3fd8005f09818e3
ParkSangPil <kevin01123@naver.com>
2021-03-25 10:28:30 +0900

    first commit

---
+++ src/p01/Comment.java
@@ -0,0 +1,16 @@
+package p01;
+
+public class Comment {
+	public static void main(String[] args) {
+		System.out.println("주석");
+//		sysout 치면 됨
+		System.out.println("한줄 주석");
+//		sout도 됨 ㅋㅋㅋ
+		System.out.println();
+		
+		/*여러줄
+		주석은
+		이렇게*/
+		System.out.println("여러줄 주석");
+	}
+}
